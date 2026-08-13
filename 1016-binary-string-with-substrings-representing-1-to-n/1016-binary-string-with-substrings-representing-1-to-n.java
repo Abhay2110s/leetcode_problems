f99c44dbe10b5d143c831e0a9b0229f131a2756d
Abhay Singh <abhaysingh14922@gmail.com>
2026-08-13 22:19:17 +0530

    Time: 0 ms (100%), Space: 42.5 MB (93.67%) - LeetHub

---
+++ 1016-binary-string-with-substrings-representing-1-to-n/1016-binary-string-with-substrings-representing-1-to-n.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean queryString(String s, int n) {
+        for(int i = 1 ;i<=n ;i++){
+            String s1 = Integer.toBinaryString(i);
+            if(!s.contains(s1)){
+                return false;
+            }
+        }
+        return true;
+    }
+}

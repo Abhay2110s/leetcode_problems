7cbf73b5cc59d52d7907e546a263c7b6ad04a03f
Abhay Singh <abhaysingh14922@gmail.com>
2026-09-20 18:49:48 +0530

    Time: 1 ms (100%), Space: 42.9 MB (10.27%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        long start = 1 , end= n;
+        while(start <= end)
+        {
+            long mid = start + (end - start )/2;
+            long count = mid * (mid + 1)/2;
+
+            if(count == n) return (int) mid;
+            else if(count<n) start = mid + 1;
+            else end = mid - 1;
+        }
+        return (int)end;
+    }
+}
